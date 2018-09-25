1a4850ce4d2be954d28bdbadb16addbdc604f8bf
yuansx <ysxiaoit@foxmail.com>
2018-09-26 00:53:01 +0800

    first program for python

---
+++ python/helloworld.py
@@ -0,0 +1,14 @@
+print("hello world")
+name="derekyuan"
+print("hello " + name)
+
+msg = "hello DEREKyuan"
+print(msg.title())
+print(msg.upper())
+print(msg.lower())
+
+msg = " This is a test for blank "
+print('[' + msg.rstrip() + ']')
+print('[' + msg.rstrip() + ']')
+print('[' + msg.lstrip() + ']')
+print('[' + msg.strip() + ']')
